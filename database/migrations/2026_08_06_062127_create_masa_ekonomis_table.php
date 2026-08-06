89ddcc890e2780fd5134cb59ee549fa03c24b1d8
Abdul Qodir <sy18o9f6@gmail.com>
2026-08-06 13:22:40 +0700

    membuat table masa ekonomis

---
+++ database/migrations/2026_08_06_062127_create_masa_ekonomis_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('masa_ekonomis', function (Blueprint $table) {
+            $table->id('id_ekonomis');
+            $table->foreignId('id_aset')->constrained('aset', 'id_aset')->onDelete('cascade');
+            $table->integer('umur');
+            $table->decimal('nilai_residu', 15, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('masa_ekonomis');
+    }
+};
